8b0dbeb2df52568fa56c6f447037a2edf5d1bf6e
kmrcellnet <bangkomar232@gmail.com>
2025-02-27 22:51:18 +0700

    Update Token.js

--- models/Token.js
+++ models/Token.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose');
+
+const TokenSchema = new mongoose.Schema({
+    user: String,
+    nomorMeter: String,
+    jumlah: Number,
+    token: String,
+    status: { type: String, default: 'pending' }, // pending, sukses
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Token', TokenSchema);
 
