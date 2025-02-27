4f6163f39e5aef81b4c1d50a1281ade0042b288d
kmrcellnet <bangkomar232@gmail.com>
2025-02-27 22:53:08 +0700

    Update tokenRoutes.js

--- routes/tokenRoutes.js
+++ routes/tokenRoutes.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { beliToken, getTokens } = require('../controllers/tokenController');
+
+router.post('/beli-token', beliToken);
+router.get('/daftar-token', getTokens);
+
+module.exports = router;
 
