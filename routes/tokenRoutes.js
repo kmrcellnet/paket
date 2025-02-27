abd888702eb58acf4a9174912eb83782cabf3694
kmrcellnet <bangkomar232@gmail.com>
2025-02-27 23:16:04 +0700

    Delete routes directory

--- routes/tokenRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { beliToken, getTokens } = require('../controllers/tokenController');
-
-router.post('/beli-token', beliToken);
-router.get('/daftar-token', getTokens);
-
-module.exports = router;
-
