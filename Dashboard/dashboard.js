5f6e393086df61b48cca00caf8136d895ff3e9d7
kmrcellnet <bangkomar232@gmail.com>
2025-03-02 04:43:11 +0700

    Update dashboard.js

--- Dashboard/dashboard.js
+++ Dashboard/dashboard.js
@@ -67,7 +67,7 @@ document.addEventListener("DOMContentLoaded", function () {
     topupForm.addEventListener("submit", function (event) {
         event.preventDefault();
         let nominal = document.getElementById("topup-nominal").value;
-        let gambar = document.getElementById("topup-gambar").value;
+        let gambar = document.getElementById("paket-gambar").value;
 
         if (nominal && gambar) {
             topupData.push({ nominal, gambar });
