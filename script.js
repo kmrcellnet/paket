48d6717d1482bb9e4ac79b769d1e083cc788762c
kmrcellnet <bangkomar232@gmail.com>
2025-03-03 01:50:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("data.json")
-        .then(response => response.json())
-        .then(data => {
-            const gameList = document.getElementById("game-list");
-            data.games.forEach(game => {
-                let div = document.createElement("div");
-                div.classList.add("game-card");
-                div.innerHTML = `
-                    <img src="assets/img.png" width="100" />
-                    <h3>${game.name}</h3>
-                    <p>Mulai dari Rp${game.price}</p>
-                `;
-                div.onclick = () => alert(`Top-Up ${game.name} dipilih!`);
-                gameList.appendChild(div);
-            });
-        })
-        .catch(error => console.error("Error loading games:", error));
-});
+function openGame(gameName) {
+    window.location.href = `checkout.html?game=${gameName}`;
+}
